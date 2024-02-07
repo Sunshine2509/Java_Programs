fe85d2a7d141e1a37798cd83c917b3c778d5670a
Pratiksha Patil <159006604+Sunshine2509@users.noreply.github.com>
2024-02-07 19:08:09 +0530

    Add files via upload

---
+++ Print_Byte_Variable.java
@@ -0,0 +1,22 @@
+class Print_Byte_Variable
+{
+	// This is the main method
+	
+	public static void main(String[]pbv)
+{
+	// Declaration and intialization of Variable
+	
+	byte Variable1=14;
+	
+	// Print the output
+	
+	System.out.print(Variable1);
+}
+
+}
+
+
+
+/*OUTPUT:
+1: Compile Time:Success
+2: RunTime: Success and 14 */
