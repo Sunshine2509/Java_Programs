52c6053f2da243846d552ac3a026da00a14cf608
Pratiksha Patil <159006604+Sunshine2509@users.noreply.github.com>
2024-02-09 00:26:02 +0530

    Double_Variable
    Printing of double type of variable

---
+++ Print_Double_Variable.java
@@ -0,0 +1,14 @@
+class Print_Double_Variable
+{
+	// Main method
+	public static void main(String [] pfv)
+	{
+		//Initialization and Declaration of variable
+		double value= 2545.5058;
+		System.out.println(value);
+	}
+}
+
+/* Output:
+1: Compile Time: Success
+2: Runtime: Success and 2545.5058 */
