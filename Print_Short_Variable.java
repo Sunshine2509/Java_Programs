1b0c85d3a5f542bb602c2aa6d1aabfc1bd061549
Pratiksha Patil <159006604+Sunshine2509@users.noreply.github.com>
2024-02-08 09:57:36 +0530

    Short_Variable
    Declaration, Initialization and Printing of short variable.

---
+++ Print_Short_Variable.java
@@ -0,0 +1,22 @@
+class Print_Short_Variable
+{
+	// This is the main method
+	
+	public static void main(String[]psv)
+{
+	// Declaration and intialization of Variable
+	
+	short Variable1=5000;
+	
+	// Print the output
+	
+	System.out.print(Variable1);
+}
+
+}
+
+
+
+/*OUTPUT:
+1: Compile Time:Success
+2: RunTime: Success and 5000 */
