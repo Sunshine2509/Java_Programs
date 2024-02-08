6cf4f3fe2d8933154d7629743160b2900da1af5b
Pratiksha Patil <159006604+Sunshine2509@users.noreply.github.com>
2024-02-09 00:29:38 +0530

    String_Variable
    Printing of string type of variable

---
+++ Print_String_Variable.java
@@ -0,0 +1,16 @@
+//Printing String variable
+class Print_String_Variable
+{
+	//Main method 
+	public static void main(String[]args)
+	{
+		String name="Pratiksha";
+		System.out.println(name);
+		System.out.println("Neha");
+	}
+}
+
+
+/* Output:
+1: Compile Time: Success
+2: Runtime: Success and Pratiksha and Neha */
