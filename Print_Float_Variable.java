35f158c3217ae415c4c6ea45795f86a4c15c059a
Pratiksha Patil <159006604+Sunshine2509@users.noreply.github.com>
2024-02-09 00:24:47 +0530

    Float_Variable
    Printing of flot Variable

---
+++ Print_Float_Variable.java
@@ -0,0 +1,14 @@
+class Print_Float_Variable
+{
+	// Main method
+	public static void main(String [] pfv)
+	{
+		//Initialization and Declaration of variable
+		float value= 25.50f;
+		System.out.println(value);
+	}
+}
+
+/* Output:
+1: Compile Time: Success
+2: Runtime: Success and 25.50*/
