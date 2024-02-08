2c7d75e458667c84c1588c943ecb406594e8a4d6
Pratiksha Patil <159006604+Sunshine2509@users.noreply.github.com>
2024-02-09 00:23:50 +0530

    Print_Int_Variable
    Printing of integer type of variable.

---
+++ Print_Int_Variable.java
@@ -0,0 +1,15 @@
+class Print_Int_Variable
+{
+	//This is the main method
+	public static void main(String []piv)
+{
+	//Initialization and declaration of int variable
+	int number=67;
+	//Print the variable
+	System.out.print(number);
+}
+}
+
+/* Output:
+1: Compile Time: Success
+2: Runtime: Success and 67 */
