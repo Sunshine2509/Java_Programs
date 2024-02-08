6f548dfa2119aaede23eb4674b869b8a45295bf2
Pratiksha Patil <159006604+Sunshine2509@users.noreply.github.com>
2024-02-09 00:27:08 +0530

    Char_Variable
    Printing of character type of variable

---
+++ Print_Char_Variable.java
@@ -0,0 +1,15 @@
+class Print_Char_Variable
+{
+	//This is the main method
+	public static void main(String[]plv)
+{
+	//Initialization and Declaration of char variable
+	char character='a';
+	//Print the variable
+	System.out.print(character);
+}
+}
+
+/* Output:
+1: Compile Time: Success
+2: Runtime: Success and a */
