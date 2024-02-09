59d49b38f8ca88ff1d83e3126c52c52db794b26c
Pratiksha Patil <159006604+Sunshine2509@users.noreply.github.com>
2024-02-09 10:26:54 +0530

    Concat_Number_

---
+++ Concat_Number_Character.java
@@ -0,0 +1,14 @@
+class Concat_Number_Character
+{
+	public static void main(String[]cnc)
+
+	{
+		int number=2001;
+		char value='a';
+		System.out.print(number+value);
+	}
+}
+
+/* Output :
+1: Compile Time: Success
+2: RunTime: Success and  */
