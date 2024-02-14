c5af4545a6e23b3c00971439ae7c4cb10747978f
Pratiksha Patil <159006604+Sunshine2509@users.noreply.github.com>
2024-02-14 23:43:51 +0530

    Eligibility_Of_Vote
    Using Relational operators like greater than or less than we can check the eligibility for votes.

---
+++ Check_Eligibility_Of_Vote.java
@@ -0,0 +1,9 @@
+class Check_Eligibility_Of_Vote
+{
+	public static void main(String[]args)
+{
+	int age=22;
+	boolean result=age>=18;
+	System.out.println("Eligibility for voting is" + result);
+}
+}
