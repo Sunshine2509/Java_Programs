3e14e8ac21d40ad8855cac339e6b57f0dc22fd63
Pratiksha Patil <159006604+Sunshine2509@users.noreply.github.com>
2024-02-14 23:52:59 +0530

    Example_Of_Comparison
    Compare different variables

---
+++ Example_Of_Comparision.java
@@ -0,0 +1,28 @@
+class Example_Of_Comparision
+{
+	//Main Method
+	public static void main(String args[])
+	{
+		// Variable declaration 
+		int age_1=25;
+		int age_2=80;
+		boolean result_1=age_1<age_2;
+		
+		//Print output
+		System.out.println("The result is " + result_1);
+
+		int Price_1=56;
+		int Price_2=58;
+		boolean result_2=Price_1<Price_2;
+
+		//Print output
+		System.out.println("The result is " + result_2);
+
+		int distance_1=15;
+		int distance_2=15;
+		boolean result_3=distance_1<distance_2;
+
+		//Print output
+		System.out.println("The result is " + result_3);
+}
+}
