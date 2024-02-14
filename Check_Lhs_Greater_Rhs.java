04b5dcc39802b00da3896dc0d119675788d7f645
Pratiksha Patil <159006604+Sunshine2509@users.noreply.github.com>
2024-02-14 23:51:40 +0530

    Comparing LHS with RHS
    Comparison using relational operator.

---
+++ Check_Lhs_Greater_Rhs.java
@@ -0,0 +1,13 @@
+class Check_Lhs_Greater_Rhs
+{
+	// Main Method
+	public static void main(String []args)
+{
+	//declaration and initialization of variable
+	int Value_1=25;
+	int Value_2=26;
+	boolean result=Value_1>Value_2;
+	//Printing output 
+	System.out.println("The result is " + result);
+}
+}
