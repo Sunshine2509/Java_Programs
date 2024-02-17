6b9b351fcc9d1eaf360cf87700c7e97098532e22
Pratiksha Patil <159006604+Sunshine2509@users.noreply.github.com>
2024-02-17 23:55:35 +0530

    Largest_Among_Two_Numbers
    Finding larger number using Ternary Operator.

---
+++ Largest_Number.java
@@ -0,0 +1,10 @@
+class Largest_Number
+{
+	//Main Method
+	public static void main(String[]args)
+	{
+		int a=10,b=20;
+		String Result=(a>b)?("a is larger"):("b is larger");
+		System.out.println(Result);
+	}
+}
