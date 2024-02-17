a1e07a02a7ce37f30e1ed2095a5ade0171c6808e
Pratiksha Patil <159006604+Sunshine2509@users.noreply.github.com>
2024-02-18 00:17:43 +0530

    Check_Vowels
    Checking the character is vowels or not using ternary operator.

---
+++ Check_Vowels.java
@@ -0,0 +1,9 @@
+class Check_Vowels
+{
+	public static void main(String[]args)
+	{
+		char ch='A';
+	String Result= (ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')?("vowels"):("Not vowel");
+System.out.println(Result);
+}
+}
