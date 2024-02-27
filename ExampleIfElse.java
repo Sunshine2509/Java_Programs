b195d8d134b4e2581567fdfc5ec5071fa1687798
Pratiksha Patil <159006604+Sunshine2509@users.noreply.github.com>
2024-02-28 00:39:57 +0530

    check vote Eligibility
    According to the age , checking vote eligibility using conditional statements (if----else).

---
+++ ExampleIfElse.java
@@ -0,0 +1,32 @@
+// Java program to check if person is eligible or not for voting
+
+class ExampleIfElse
+{
+	// Main method
+
+	public static void main(String args[])
+	{
+		//Declaration and initialization of variable
+
+		int age=25;
+
+		// check condition using conditional statements
+
+		if(age>=18)
+	{
+		System.out.println("Congrats");   // if block
+	}
+
+		else
+		{
+		System.out.println("Next time");  //else block
+ 	        }
+	}
+}
+
+
+	/* Output: 
+		   Compile time: Success
+
+		   Runtime : Sucess and Congrats*/
+		  
