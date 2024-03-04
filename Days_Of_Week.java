87dc7aead81376edfffa83b42dcbc436581b6305
Pratiksha Patil <159006604+Sunshine2509@users.noreply.github.com>
2024-03-04 22:55:13 +0530

    switch statement ( Fall through )

---
+++ Days_Of_Week.java
@@ -0,0 +1,21 @@
+// Java program to print days in a week using switch statement.
+// This is the example of Fallthrough 
+
+class Days_Of_Week
+{
+	public static void main(String args[])
+	{
+		int dayNum=5;
+		switch(dayNum)
+		{
+		case 1 : System.out.println("Monday");
+		case 2 : System.out.println("Tuesday");
+		case 3 : System.out.println("Wednesday");
+		case 4 : System.out.println("Thursday");
+		case 5 : System.out.println("Friday");
+		case 6 : System.out.println("Saturday");
+		case 7 : System.out.println("Sunday");
+		default: System.out.println(" Invalid Day Number.");
+		}
+	}
+}
