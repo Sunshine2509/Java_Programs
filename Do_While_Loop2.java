d77122c392a58e8778fc6b40b7f7e5d31c57e8de
Pratiksha Patil <159006604+Sunshine2509@users.noreply.github.com>
2024-03-06 00:21:10 +0530

    Print table of 2

---
+++ Do_While_Loop2.java
@@ -0,0 +1,34 @@
+// Java Program, to print the table of 2 using do while loop.
+
+class Do_While_Loop2
+{
+	// Main method
+	public static void main(String args[])
+	{
+		// Declaration and initialization of variable
+
+		int n=1;
+		
+		// do while loop
+		do
+		{
+			System.out.println(2*n);
+			n++;
+		}while(n<=10);
+	}
+}
+
+	/* Output: 
+
+	Compile Time: Success
+	Runtime : Success and
+		2
+		4
+		6
+		8
+		10
+		12
+		14
+		16
+		18
+		20 */
