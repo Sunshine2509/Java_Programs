929d9097d50dfe862ab7a0f3d80441d5d57212a4
Pratiksha Patil <159006604+Sunshine2509@users.noreply.github.com>
2024-03-06 00:16:47 +0530

    Print the numbers from 1 to 10

---
+++ Do_While_Loop1.java
@@ -0,0 +1,35 @@
+// Java Program, to print the numbers from 1 to 10 using do while loop.
+
+class Do_While_Loop1
+{
+	// Main method
+	public static void main(String args[])
+	{
+		// Declaration and initialization of variable
+
+		int n=1;
+		
+		// do while loop
+		do
+		{
+			System.out.println(n);
+			n++;
+		}while(n<=10);
+	}
+}
+
+	/* Output: 
+
+	Compile Time: Success
+	Runtime : Success and
+		1
+		2
+		3
+		4
+		5
+		6
+		7
+		8
+		9
+		10 */
+		
