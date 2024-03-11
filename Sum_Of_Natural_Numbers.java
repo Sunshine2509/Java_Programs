7dfc54a81220346a57616f05e01bb30c37926c02
Pratiksha Patil <159006604+Sunshine2509@users.noreply.github.com>
2024-03-12 00:02:09 +0530

    Sum of natural numbers without using loop

---
+++ Sum_Of_Natural_Numbers.java
@@ -0,0 +1,20 @@
+/* write a programm to print sum of all natural number  till  user given number */
+
+
+class Sum_Of_Natural_Numbers
+{
+	//Main method
+	public static void main(String args[])
+	{
+		int n=10;
+		int sum=n*(n+1)/2;
+		System.out.println(sum);
+	}
+}
+
+
+	/* Output: 
+
+	Compile Time: Success 
+
+	Runtime : Success and 55 */
